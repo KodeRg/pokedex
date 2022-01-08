ff6d2ac2e992aed0da629a1d4f766b6faa47cf6c
Lucas Cabral <84217024+KodeRg@users.noreply.github.com>
2022-01-08 12:06:24 -0300

    Delete Api.js

--- Pokedex/Api.js
+++
@@ -1,6 +0,0 @@
-const realizarPeticion = async () =>{
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await res.json();
-    console.log (data);
-};
-realizarPeticion();
